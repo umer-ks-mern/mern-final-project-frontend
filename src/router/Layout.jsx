51f9b2e76d797e259da1dacb30099ff0801b7c83
Zainab <zainabbhatti808@gmail.com>
2023-09-23 13:06:07 +0500

    final commit

--- src/router/Layout.jsx
+++ src/router/Layout.jsx
@@ -1,5 +1,5 @@
 import CustomerIndex from "../modules/customer/pages/CustomerIndex";
-import { sellerRoutes } from "../modules/seller/routes";
+//import { sellerRoutes } from "../modules/seller/routes";
 
 export const LayoutRouter = [
   { path: "layout-route", element: <h1>Layout</h1> },
