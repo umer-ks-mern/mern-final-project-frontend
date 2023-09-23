a66612656304296b4c85348ac67f432b4c43a76b
Muhammad Taimoor Khan <62849524+taimoor55971@users.noreply.github.com>
2023-09-24 04:05:31 +0500

    Dynamic Table and Form created Admin panel UI completed /Layout/Form.layout.jsx /Layout/Table.layout.jsx added ViewProducts ViewOrders and ViewUsers modified

--- src/modules/seller/pages/viewuser.jsx
+++ src/modules/seller/pages/viewuser.jsx
@@ -1,37 +1,28 @@
 import React, { useState } from 'react';
 import { useEffect,usestate } from 'react';
 import axios from 'axios';
+import DynamicTable from '../../../layout/Table.layout';
 
 const ViewUser=()=>{
 
     const [users,setUsers]=useState([]);
  useEffect(()=>{
-    axios.get("https://localhost:3300/users").then((res)=>{
+    axios.get("http://localhost:3300/users").then((res)=>{
         setUsers(res?.data)
     }).catch((err)=>{
         console.log(err)
     })
  },[])
 
+ const columns=[
+  { id: "name", label: "User Name" },
+  { id: "email", label: "Email" },
+  {id:'phone',label:'Phone'}
+ ]
+
     return (
         <>
-        <div className="bg-gray-100 p-4">
-      <h1 className="text-2xl font-semibold mb-4">User List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {users.map((user) => (
-          <div
-            key={user.id}
-            className="bg-white p-4 rounded-lg shadow-md"
-          >
-            <h2 className="text-xl font-semibold">{user.name}</h2>
-            <p className="text-gray-600">{user.email}</p>
-            <p className="text-gray-600">{user.phone}</p>
-            {/* Add more user information here */}
-          </div>
-        ))}
-      </div>
-    </div>
-        
+      <DynamicTable data={users} columns={columns} rowsPerPageOptions={[5,10,15]} defaultRowsPerPage={5}/>
         </>
     )
 };
