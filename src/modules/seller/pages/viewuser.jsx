cce75bb32d6545f837dc7b9e4944a36d2841ab80
Umer <aehtesham086@gmail.com>
2023-09-24 22:51:50 +0500

    merged code migrated to vite

--- src/modules/seller/pages/viewuser.jsx
+++ src/modules/seller/pages/viewuser.jsx
@@ -1,32 +1,37 @@
-import React, { useState } from 'react';
-import { useEffect,usestate } from 'react';
-import axios from 'axios';
-import DynamicTable from '../../../layout/Table.layout';
+import React, { useState } from "react";
+import { useEffect, usestate } from "react";
+import axios from "axios";
+import DynamicTable from "../../../layout/Table.layout";
 
-const ViewUser=()=>{
+const ViewUser = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3300/users")
+      .then((res) => {
+        setUsers(res?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    const [users,setUsers]=useState([]);
- useEffect(()=>{
-    axios.get("http://localhost:3300/users").then((res)=>{
-        setUsers(res?.data)
-    }).catch((err)=>{
-        console.log(err)
-    })
- },[])
+  const columns = [
+    { id: "name", label: "User Name" },
+    { id: "email", label: "Email" },
+    { id: "phone", label: "Phone" },
+  ];
 
- const columns=[
-  { id: "name", label: "User Name" },
-  { id: "email", label: "Email" },
-  {id:'phone',label:'Phone'}
- ]
-
-    return (
-        <>
-        
-      <DynamicTable data={users} columns={columns} rowsPerPageOptions={[5,10,15]} defaultRowsPerPage={5}/>
-        </>
-    )
+  return (
+    <>
+      <DynamicTable
+        data={users}
+        columns={columns}
+        rowsPerPageOptions={[5, 10, 15]}
+        defaultRowsPerPage={5}
+      />
+    </>
+  );
 };
-
 
 export default ViewUser;
